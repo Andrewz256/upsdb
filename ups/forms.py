d9da9033c56be0330b37c672a608c15882ba45ad
Telemat <provision@obit.ru>
2018-01-11 14:07:18 +0300

    clean

--- ups/forms.py
+++ ups/forms.py
@@ -6,7 +6,6 @@ class PostUPS(forms.ModelForm):
 	class Meta:
 	    model = UPSB
 	    fields = ('modUps', 'dateU', 'obitN', 'commentU', 'state', 'obitInsBat1', 'obitInsBat2', 'obitOutBat1', 'obitOutBat2','released',)
-
 class PostBat(forms.ModelForm):
 	class Meta:
 	    model = Battery
