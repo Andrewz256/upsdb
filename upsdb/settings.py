63789ed2adeadcad6e7636312c5a7736cdfc183e
Telemat <provision@obit.ru>
2018-01-12 08:59:55 +0300

    edit settings.py

--- upsdb/settings.py
+++ upsdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h=%%z%tpye(bc3^gc7%*ujpjyp3@a)s$kpba(inu83+a6))rl8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.4.228', '127.0.0.1']
 
 
 # Application definition
