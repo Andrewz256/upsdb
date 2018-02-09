d217ac9cb3aa7d5536e1ab7ac5d8685a0a97f196
Andrei Zmeevskiy <zmeevskiy@obit.local>
2018-02-09 14:11:00 +0300

    test brach

--- ups/forms.py
+++ ups/forms.py
@@ -3,18 +3,18 @@ from .models import UPSB, Battery
 
 
 class PostUPS(forms.ModelForm):
-	class Meta:
-	    model = UPSB
-	    fields = ('modUps', 'dateU', 'obitN', 'state', 'obitInsBat1', 'obitInsBat2', 'obitOutBat1', 'obitOutBat2','released',  'commentU',)
-	    widgets = {
+    class Meta:
+        model = UPSB
+        fields = ('modUps', 'dateU', 'obitN', 'state', 'obitInsBat1', 'obitInsBat2', 'obitOutBat1', 'obitOutBat2','released',  'commentU',)
+        widgets = {
 	        # 'obitInsBat1': forms.TextInput(),
 	        # 'obitInsBat2': forms.TextInput(),
 	        # 'obitOutBat1': forms.TextInput(),
 	        # 'obitOutBat2': forms.TextInput(),
 	        'commentU': forms.Textarea(),
 	    }
-	    labels = {
-	      'obitInsBat1': "Insert_Battery_1", 
+        labels = {
+	      'obitInsBat1': "Insert_Battery_1",
 	      'obitInsBat2': "Insert_Battery_2",
 	      'obitOutBat1': "Remove_Battery_1",
 	      'obitOutBat2': "Remove_Battery_1",
@@ -22,16 +22,16 @@ class PostUPS(forms.ModelForm):
 	      'modUps': "Model",
 	      'released': "in stock?",
 	      'commentU': "Comment",
-	      'dateU': "Date", 
+	      'dateU': "Date",
 	    }
 class PostBat(forms.ModelForm):
-	class Meta:
-	    model = Battery
-	    fields = ('dateB','obitBN', 'modBat', 'state', 'commentB',)
-	    widgets = {
+    class Meta:
+         model = Battery
+         fields = ('dateB','obitBN', 'modBat', 'state', 'commentB',)
+         widgets = {
 	       'commentB': forms.Textarea(),
 	    }
-	    labels = {
+         labels = {
 	       'commentB': "Comment",
 	       'dateB': "Date",
 	       'obitBN': "Obit ##",
--- ups/models.py
+++ ups/models.py
@@ -7,51 +7,51 @@ from django.utils import timezone
 # Create your models here.
 
 class State(models.Model):
-	stateUB=models.CharField(max_length=20,unique=True, null=False,blank=False)
-	
-	def __str__(self):
-		return self.stateUB
+    stateUB=models.CharField(max_length=20,unique=True, null=False,blank=False)
+
+    def __str__(self):
+        return self.stateUB
 
 class Modbat(models.Model):
-	modBat=models.CharField(max_length=20,unique=True, null=False,blank=False)
-	
-	def __str__(self):
-		return self.modBat
+    modBat=models.CharField(max_length=20,unique=True, null=False,blank=False)
+
+    def __str__(self):
+        return self.modBat
 
 class Modups(models.Model):
-	modUps=models.CharField(max_length=20,unique=True, null=False,blank=False)
-	
-	def __str__(self):
-		return self.modUps
+    modUps=models.CharField(max_length=20,unique=True, null=False,blank=False)
+
+    def __str__(self):
+        return self.modUps
 
 class Battery(models.Model):
-	modBat = models.ForeignKey(Modbat,related_name="+",to_field="modBat", on_delete=models.SET_NULL,null=True)
-	obitBN = models.CharField(max_length=20,primary_key=True,null=False)
-	state = models.ForeignKey(State,related_name="+",to_field="stateUB", on_delete=models.SET_NULL,null=True)
-	dateB = models.DateTimeField(default=timezone.now)
-	commentB = models.CharField(max_length=50,blank=True)
-	
-	def __str__(self):
-		return self.obitBN
-	def publishb(self):
-		self.dateB = timezone.now()
-		self.save()
-	
+    modBat = models.ForeignKey(Modbat,related_name="+",to_field="modBat", on_delete=models.SET_NULL,null=True)
+    obitBN = models.CharField(max_length=20,primary_key=True,null=False)
+    state = models.ForeignKey(State,related_name="+",to_field="stateUB", on_delete=models.SET_NULL,null=True)
+    dateB = models.DateTimeField(default=timezone.now)
+    commentB = models.CharField(max_length=50,blank=True)
+
+    def __str__(self):
+        return self.obitBN
+    def publishb(self):
+        self.dateB = timezone.now()
+        self.save()
+
 class UPSB(models.Model):
-	modUps = models.ForeignKey(Modups,related_name="+",to_field="modUps", on_delete=models.SET_NULL,null=True,blank=False)
-	obitN = models.CharField(max_length=20,blank=False)
-	obitInsBat1 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
-	obitInsBat2 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
-	obitOutBat1 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
-	obitOutBat2 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
-	state = models.ForeignKey(State, related_name="+", to_field="stateUB", on_delete=models.SET_NULL,null=True)
-	released = models.BooleanField(default=False)
-	dateU = models.DateTimeField(default=timezone.now)
-	commentU = models.CharField(max_length=100,blank=True)
-	
-	def __str__(self):
-                return self.obitN
-	def publishu(self):
-		self.dateU = timezone.now()
-		self.save()
+    modUps = models.ForeignKey(Modups,related_name="+",to_field="modUps", on_delete=models.SET_NULL,null=True,blank=False)
+    obitN = models.CharField(max_length=20,blank=False)
+    obitInsBat1 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
+    obitInsBat2 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
+    obitOutBat1 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
+    obitOutBat2 = models.ForeignKey(Battery, related_name="+", on_delete=models.SET_NULL,null=True,blank=True)
+    state = models.ForeignKey(State, related_name="+", to_field="stateUB", on_delete=models.SET_NULL,null=True)
+    released = models.BooleanField(default=False)
+    dateU = models.DateTimeField(default=timezone.now)
+    commentU = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+      return self.obitN
+    def publishu(self):
+      self.dateU = timezone.now()
+      self.save()
 
